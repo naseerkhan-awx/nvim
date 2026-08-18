73716095fbb443fe67b45b07018e3c6fac2e7d15
Naseer Khan <naseer.khan@airwallex.com>
2026-08-18 15:56:51 +0800

    modfied git commands

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,6 +4,7 @@ return {
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
     keys = {
       { "<D-S-g>", "<cmd>DiffviewOpen<cr>", desc = "DiffView Open", mode = { "n", "i", "v" } },
+      { "<D-y>", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffView File History" },
       { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "DiffView File History" },
       { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "DiffView Branch History" },
     },
@@ -14,11 +15,17 @@ return {
         },
         file_panel = {
           { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          { "n", "X", false },
         },
         file_history_panel = {
           { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
         },
       },
     },
+    config = function(_, opts)
+      local restore_entry = require("diffview.config").actions.restore_entry
+      table.insert(opts.keymaps.file_panel, { "n", "d", restore_entry, { desc = "Restore entry" } })
+      require("diffview").setup(opts)
+    end,
   },
 }
