87053afe5db3df0ebabeb72110bf0b7492bb7023
naseer2426 <naseerfathima2426@gmail.com>
2026-08-22 16:59:23 +0800

    added more configurations to code diff

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -9,12 +9,35 @@ return {
       { "<leader>gH", "<cmd>CodeDiff history<cr>", desc = "CodeDiff Branch History" },
       { "<leader>gm", "<cmd>CodeDiff merge %<cr>", desc = "CodeDiff Resolve Conflicts" },
     },
+    init = function()
+      local group = vim.api.nvim_create_augroup("CodeDiffListNavigation", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = { "codediff-explorer", "codediff-history" },
+        callback = function(event)
+          vim.keymap.set("n", "l", "<CR>", {
+            buffer = event.buf,
+            remap = true,
+            silent = true,
+            desc = "Select CodeDiff item",
+          })
+        end,
+      })
+    end,
     opts = {
+      keymaps = {
+        view = {
+          toggle_explorer = "<D-b>",
+        },
+        explorer = {
+          restore = "d",
+        },
+      },
       diff = {
         layout = "side-by-side",
         conflict_ours_position = "left",
         conflict_result_position = "center",
-        conflict_result_width_ratio = { 1, 2, 1 },
+        conflict_result_width_ratio = { 1, 1, 1 },
       },
     },
   },
