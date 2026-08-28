ced7a57f1823a1477891811643d55cf752d21340
naseer.khan <naseer.khan@airwallex.com>
2026-08-28 11:52:08 +0800

    added tab title

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,15 @@
 
 -- Keep some padding like VS Code
 vim.opt.scrolloff = 5
+
+-- Keep the terminal tab title fixed to the session's starting target.
+local session_title = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+if vim.fn.argc() == 1 then
+  local target = vim.fn.argv(0)
+  if vim.fn.isdirectory(target) == 0 then
+    session_title = vim.fn.fnamemodify(target, ":t")
+  end
+end
+
+vim.opt.title = true
+vim.opt.titlestring = session_title
